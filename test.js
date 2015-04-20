9612ec4207c214353dbcacaf7c3d862967e30abf
Matt Surabian <matt@mattsurabian.com>
2015-04-20 11:58:53 -0400

    Update test text to reflect changes to decrypt

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
+'use strict';
+
 var test = require('tape');
 var aesgcm = require('./');
 
-test('basic', function (t) {
+test('basic', function(t) {
   t.plan(1);
   var config = {
     key: aesgcm.createKeyBuffer()
@@ -12,21 +14,23 @@ test('basic', function (t) {
 
   var data = [
     'Everything that is written into the stream will be encrypted.\n',
-    'But it will be held in memory as encrypted cipher text.\n',
-    'It won\'t continue to be streamed until end is called or triggered.\n',
-    'That\'s because GCM creates a MAC based on ALL the cipher text.\n',
-    'Authentication requires the MAC and Nonce BEFORE decryption begins.\n',
-    'That\'s why the first 28 bytes from encrypt are the Nonce and MAC.\n'
+    'But because GCM creates a MAC based on ALL the cipher text,\n',
+    'it\'s necessary to explicitly call end when writing to the stream.\n',
+    'Otherwise you won\'t be able to authenticate and decrypt the data.\n',
+    'The decrypter relies on the first 12 bytes of the cipher text being the nonce,\n',
+    'and the last 16 bytes of the cipherText being the MAC;\n',
+    'which is only generated and sent on flush.\n'
   ];
+
   var outData = [];
-  encrypt.pipe(decrypt).on('data', function (d) {
+  encrypt.pipe(decrypt).on('data', function(d) {
     outData.push(d.toString());
-  }).on('finish', function () {
+  }).on('finish', function() {
     t.equals(data.join(''), outData.join(''));
-  }).on('error', function (e) {
+  }).on('error', function(e) {
     t.error(e);
   });
-  data.forEach(function (s) {
+  data.forEach(function(s) {
     encrypt.write(s);
   });
   encrypt.end();
